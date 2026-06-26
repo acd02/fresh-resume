99e597334a42bf0580be0b47230f1da6a9b72267
acd02 <alexandre.cadiot@adevinta.com>
2026-06-26 10:08:31 +0200

    feat: enable view transitions

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,5 +1,6 @@
 import { define } from "@/utils.ts";
 import { Header } from "@/components/Header.tsx";
+import { Partial } from "fresh/runtime";
 import { Footer } from "@/components/Footer.tsx";
 
 export default define.page(function App({ Component, url }) {
@@ -11,20 +12,29 @@ export default define.page(function App({ Component, url }) {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Alexandre Cadiot — wesh</title>
-        <meta name="description" content="Personal website and resume of Alexandre Cadiot, Staff Frontend Engineer." />
+        <meta
+          name="description"
+          content="Personal website and resume of Alexandre Cadiot, Staff Frontend Engineer."
+        />
         {pathname === "/cv" && <meta name="robots" content="noindex" />}
       </head>
-      <body class="flex h-full bg-zinc-50 dark:bg-black">
+      <body
+        f-client-nav
+        f-view-transition
+        class="flex h-full bg-zinc-50 dark:bg-black"
+      >
         <div class="fixed inset-0 flex justify-center sm:px-8 print:hidden">
           <div class="flex w-full max-w-7xl lg:px-8">
             <div class="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
           </div>
         </div>
         <div class="relative flex w-full flex-col">
-          <Header pathname={pathname} />
-          <main class="flex-auto">
-            <Component />
-          </main>
+          <Partial name="main">
+            <Header pathname={pathname} />
+            <main class="flex-auto">
+              <Component />
+            </main>
+          </Partial>
           <Footer />
         </div>
       </body>
